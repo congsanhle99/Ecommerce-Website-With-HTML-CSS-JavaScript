1d1a2f0aa6508c585da15695a43506effbffc4d0
CongSanhLe <congsanhle1999@gmail.com>
2022-08-29 17:39:28 +0700

    [1-2] part 1 - 2 home shop detail product resp

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,36 @@
-console.log("aaaaa");
+// index
+const bar = document.querySelector("#bar");
+const navbar = document.querySelector("#navbar");
+const close = document.querySelector("#close");
+
+// click product in shop direct to product detail
+const products = document.querySelectorAll(".pro");
+
+// product details
+const MainImg = document.querySelector("#MainImg");
+const smallImgs = document.querySelectorAll(".small-img");
+
+// index
+if (bar) {
+  bar.addEventListener("click", () => {
+    navbar.style.right = 0;
+  });
+}
+
+if (close) {
+  close.addEventListener("click", () => {
+    navbar.style.right = `${-300}px`;
+  });
+}
+
+// click product in shop direct to product detail
+products[0].addEventListener("click", () => {
+  window.location.href = "sproduct.html";
+});
+
+// product details
+smallImgs.forEach((smallImg) => {
+  smallImg.addEventListener("click", () => {
+    MainImg.src = smallImg.src;
+  });
+});
