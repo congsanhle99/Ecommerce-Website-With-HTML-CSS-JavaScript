95853813f71adba204696047954eab0d976c9cb3
CongSanhLe <congsanhle1999@gmail.com>
2022-08-30 16:32:24 +0700

    [3-4-5-6] about blog contact cart responsive

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,6 +1,7 @@
 // index
 const bar = document.querySelector("#bar");
 const navbar = document.querySelector("#navbar");
+const navbarLis = document.querySelectorAll("#navbar li a");
 const close = document.querySelector("#close");
 
 // click product in shop direct to product detail
@@ -24,8 +25,10 @@ if (close) {
 }
 
 // click product in shop direct to product detail
-products[0].addEventListener("click", () => {
-  window.location.href = "sproduct.html";
+products.forEach((product) => {
+  product.addEventListener("click", () => {
+    window.location.href = "sproduct.html";
+  });
 });
 
 // product details
@@ -33,4 +36,14 @@ smallImgs.forEach((smallImg) => {
   smallImg.addEventListener("click", () => {
     MainImg.src = smallImg.src;
   });
+});
+
+// active tag
+const activePage = window.location.pathname;
+navbarLis.forEach((navbarLi) => {
+  if (navbarLi.href.includes(`${activePage}`)) {
+    navbarLi.classList.add("active");
+  } else {
+    navbarLi.classList.remove("active");
+  }
 });
